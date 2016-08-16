9155afa02d2259de369c30b24f935addab03e04a
Harry <harry@Harry.local>
2016-08-16 23:55:09 +0100

    AHHHH added correct procfile

--- home/views.py
+++ home/views.py
@@ -49,46 +49,46 @@ def contact(request):
   return HttpResponse(template.render(context))
 
 
-def submit(request):
-  if request.method == 'POST':
+# def submit(request):
+#   if request.method == 'POST':
 
-    # print 'post'
+#     # print 'post'
 
-    form = EmailForm(request.POST)
+#     form = EmailForm(request.POST)
 
-    # if form.is_valid():
-    #   print 'valid'
-    # else:
-    #   print'not valid'
-
-
-    if form.is_valid():
-      name = request.POST.get ('name', '')
-      email = request.POST.get ('email', '')
-
-      update = request.POST.get ('update', '')
-      local = request.POST.get ('local', '')
-      invest = request.POST.get ('invest', '')
-
-      message = request.POST.get ('message', '')
-
-      date = timezone.now()
-
-      mail_obj = Info(name_text = name, email_text = email, pub_date = date, message_text = message, update_bool = update, local_bool = local, invest_bool = invest)
-      mail_obj.save()
-
-      # subject = 'Holy shit someone signed up'
-      # message = 'yeh, I know wtf are they thinking?'
-      # send_mail(subject, message, settings.EMAIL_HOST_USER,[settings.EMAIL_HOST_USER], fail_silently=False)
+#     # if form.is_valid():
+#     #   print 'valid'
+#     # else:
+#     #   print'not valid'
 
 
-      return HttpResponseRedirect(reverse('get_emails:submit'))
+#     if form.is_valid():
+#       name = request.POST.get ('name', '')
+#       email = request.POST.get ('email', '')
+
+#       update = request.POST.get ('update', '')
+#       local = request.POST.get ('local', '')
+#       invest = request.POST.get ('invest', '')
+
+#       message = request.POST.get ('message', '')
+
+#       date = timezone.now()
+
+#       mail_obj = Info(name_text = name, email_text = email, pub_date = date, message_text = message, update_bool = update, local_bool = local, invest_bool = invest)
+#       mail_obj.save()
+
+#       # subject = 'Holy shit someone signed up'
+#       # message = 'yeh, I know wtf are they thinking?'
+#       # send_mail(subject, message, settings.EMAIL_HOST_USER,[settings.EMAIL_HOST_USER], fail_silently=False)
 
 
-  else:
-    form = EmailForm()
+#       return HttpResponseRedirect(reverse('get_emails:submit'))
 
-  return render(request, 'get_emails/index.html', {
 
-    'form': form
-    } )
+#   else:
+#     form = EmailForm()
+
+#   return render(request, 'get_emails/index.html', {
+
+#     'form': form
+#     } )
